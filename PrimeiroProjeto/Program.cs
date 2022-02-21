578d752e7cadda143a8cbaa0610c2ed180a15935
Ana Paula Duarte <74932085+apduartte@users.noreply.github.com>
2022-02-21 15:48:55 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Primeiro Projeto Visual Studio");
+            Console.WriteLine("a execução acabou. tecle enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
